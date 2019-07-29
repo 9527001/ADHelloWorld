28771136fdd2409c07600e13bf1531413934aa61
JTDDong <zhaoyd@jvtd.cn>
2019-07-29 13:47:03 +0800

    测试资源文件的引用情况 1.加入测试控制器以及数字表示的图片

---
+++ ADHelloWorld/ADHelloWorld/Classes/TestViewController.h
@@ -0,0 +1,17 @@
+//
+//  TestViewController.h
+//  ADHelloWorld
+//
+//  Created by 赵东 on 2019/7/29.
+//  Copyright © 2019 赵东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
--- ADHelloWorld/ADHelloWorld/ViewController.m
+++ ADHelloWorld/ADHelloWorld/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#import "TestViewController.h"
+
 @interface ViewController ()
 
 @end
@@ -17,6 +19,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    TestViewController * testVC = [[TestViewController alloc] init];
+    [self presentViewController:testVC animated:YES completion:nil];
 }
 
 
